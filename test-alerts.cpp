8c20de061d833ab0fc6182903dbb4097056ce4f2
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-02-23 16:51:24 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,19 +3,6 @@
 #include "test/catch.hpp"
 #include "TempCheckAndAlert.h"
 
-actionList fpArrayOfActions[ALERT_TARGET_LENGTH] = {sendToController,sendToEmail};
-
-
-
-
-
-
-TEST_CASE("Sending the Breach information to EMAIL")
-{
-  REQUIRE(sendToEmail(TOO_HIGH) == TO_EMAIL);
-  REQUIRE(sendToEmail(TOO_LOW) == TO_EMAIL);
-  
-}
 
 
 
