9649d3c48680ae772d87e1a35cdd81ce07ace92c
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-02-23 15:35:50 +0530

    Added hte files where code seperation is done

---
+++ TempCheckAndAlert.c
@@ -0,0 +1,63 @@
+#include "TempCheckAndAlert.h"
+
+
+BreachType inferBreach(double value, double lowerLimit, double upperLimit) 
+{
+  BreachType lowerBreachResult = checkLowerLimit( value,  lowerLimit);
+  BreachType upperBreachResult = checkUpperLimit( value,  upperLimit);
+  
+  return lowerBreachResult + upperBreachResult;
+ 
+}
+
+BreachType classifyTemperatureBreach(
+  CoolingType coolingType, double temperatureInC) {
+ 
+  int lowerLimit = 0;
+  int upperLimit = 0;
+
+ upperLimit = passiveCoolingUpperLimit( coolingType,  upperLimit);
+ lowerLimit = passiveCoolingLowerLimit( coolingType,  lowerLimit);
+ upperLimit = hiActiveCoolingUpperLimit( coolingType,  upperLimit);
+ lowerLimit = hiActiveCoolingLowerLimit( coolingType,  lowerLimit);
+ upperLimit = medActiveCoolingUpperLimit( coolingType,  upperLimit);
+ lowerLimit = medActiveCoolingLowerLimit( coolingType,  lowerLimit);
+  
+  return inferBreach(temperatureInC, lowerLimit, upperLimit);
+}
+
+AlertTarget checkAndAlert(
+    AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC, actionList *alertAction ) 
+{
+  
+	 BreachType breachType = classifyTemperatureBreach(
+		batteryChar.coolingType, temperatureInC
+	  );
+
+	 AlertTarget targetResponse =  (*alertAction[alertTarget])(breachType);
+	  
+ }
+ 
+AlertTarget  printAlert(char* alertInfo, AlertTarget target)
+ {
+     puts(alertInfo);
+     return target;
+ }
+
+
+AlertTarget sendToController(BreachType breachType) 
+{     AlertTarget targetResponse;
+	 const unsigned short header = 0xfeed;
+	 char alertInfo[100];
+	 sprintf(alertInfo ,"%x : %x\n", header, breachType);
+	  return printAlert(alertInfo, TO_CONTROLLER );
+	 
+}
+
+AlertTarget sendToEmail(BreachType breachType) {
+  const char* recepient = "a.b@c.com";
+  char alertInfo[100];
+  sprintf(alertInfo, "To: %s\n Hi,The Temperature is %s \n", recepient ,Breachinfo[breachType] );
+  return printAlert(alertInfo, TO_EMAIL );;
+}
+
